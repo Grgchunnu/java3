850f97a9cae671841a2a92fd79341c95d919ad34
Grgchunnu <chunnugrg94@gmail.com>
2023-02-05 22:10:01 -0500

    my java3 file

---
+++ javaday3.java
@@ -0,0 +1,70 @@
+package one;
+
+public class javaday2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String city="jaipura";
+		// 0 1 2 3 4 5 6 
+		// j a i p u r a
+		
+		int len =city.length();
+		System.out.println(len);
+		
+		for (int i = 0; i < len; i++) {
+			System.out.println(city.charAt(i));
+			//i-----0
+			//i-----1
+			//i-----2
+			//i-----3
+			//i-----4
+			//i-----5
+			//i-----6
+			
+		}
+		
+		
+		//Reverse of above string
+		 String city1="jaipura";
+		 int length =city1.length();
+		 System.out.println(length);
+		 
+		
+		  for (int a = 6; a > length; a++) {
+			  System.out.println(city1.charAt(a));
+		  }
+		 
+		 
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
